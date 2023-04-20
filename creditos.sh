b99199e883faad7f609d3d00a3d8532b7152ac57
mirnamarquez2002 <113308948+mirnamarquez2002@users.noreply.github.com>
2023-04-20 00:06:08 -0600

    Add files via upload

---
+++ creditos.sh
@@ -0,0 +1,12 @@
+clear
+echo -e "\e[97;41m*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\e[0m"
+echo -e "\e[97;41m| El essarrollo de esta terminal |\e[0m"
+echo -e "\e[97;41m|    fue posible gracias a:    |\e[0m" 
+echo -e "\e[97;41m*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\e[0m"
+echo -e "\e[97;41m| Márquez Sánchez Mirna Daniela |\e[0m"
+echo -e "\e[97;41m|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\e[0m"
+echo -e "\e[97;41m| Roldán Montero Hugo Alejandro |\e[0m"
+echo -e "\e[97;41m*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\e[0m"
+echo -e "\e[97;41m| Gracias por usar nuestra terminal |\e[0m"
+echo -e "\e[97;41m|          Hasta pronto! :)  |\e[0m"
+echo -e "\e[97;41m*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\e[0m"
