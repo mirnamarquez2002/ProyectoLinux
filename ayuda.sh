33f3bf86bba6ff44ccc9191b95326bdcf50f401e
HugoARM <89823278+HugoARM@users.noreply.github.com>
2023-04-21 12:05:26 -0600

    Update ayuda.sh

--- ayuda.sh
+++ ayuda.sh
@@ -9,7 +9,6 @@ echo -e "${azul}ayuda${reset_color}: Proporciona información acerca de los coma
 echo -e "${azul}infosis${reset_color}: Proporciona información del sistema" 
 echo -e "${azul}infodia${reset_color}: Proporciona la fecha y hora actuales"
 echo -e "${azul}busca${reset_color}: Busca un archivo en un directorio específico"
-echo -e "${azul}jugar${reset_color}: Ejecuta el juego del sistema"
 echo -e "${azul}creditos${reset_color}: Muestra los créditos del programa"
 echo -e "${azul}MP3${reset_color}: Abrirá un reproductor de mp3"
 echo -e "${azul}gato${reset_color}: Abrirá el juego gato"
