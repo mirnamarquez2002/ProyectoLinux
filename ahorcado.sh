251dc4e3b3d41c39ef66af90d7e8fbedca4804ad
mirnamarquez2002 <113308948+mirnamarquez2002@users.noreply.github.com>
2023-04-22 09:51:29 -0600

    Update ahorcado.sh
    Nuestras versiones pasadas de ahorcado al introducir n número de errores, nos mostraba después que todo era error y entraba a un bucle infinito, entonces lo mejoramos e incluso hicimos una interfaz más gráfica y descriptiva para el usuario.

--- ahorcado.sh
+++ ahorcado.sh
@@ -1,52 +1,126 @@
 #!/bin/bash
 
-#Palabras creadas
-palabras=("raton" "hormiga" "perro" "gato" "caballo" "elefante" "girafa" "tigre" "leon" "aguila")
-
-# Elegir una palabra al azar
-palabrarandom=$(( RANDOM % ${#palabras[@]} ))
-palabra="${palabras[$palabrarandom]}"
-largo=${#palabra}
-
-
-adivinarpalabra=""
-for (( i=0; i<$largo; i++ )); do
-    adivinarpalabra+="_"
-done
-
-intentos_restantes=7
-palabras_adivinadas=""
-
-while [[ $adivinarpalabra != $palabra && $itentosrestantes -gt 0 ]]; do
-    echo "Palabra a adivinar: $adivinarpalabra"
-    echo "Letras ya intentadas: $palabras_adivinadas"
-    echo "Intentos restantes: $itentosrestantes"
-    read -p "Introduce una letra: " -n 1 -r
-    echo
-
-    if [[ $palabras_adivinadas == "$REPLY" ]]; then
-        echo "Ya intentaste con esa letra, prueba otra."
-        continue
-    fi
-
-    palabras_adivinadas+="$REPLY"
-
-    if [[ $palabra == "$REPLY" ]]; then
-        for (( i=0; i<$largo; i++ )); do
-            if [[ ${palabra:$i:1} == $REPLY ]]; then
-                adivinarpalabra="${adivinarpalabra:0:$i}$REPLY${adivinarpalabra:$((i+1))}"
-            fi
-        done
-        echo "¡BIEN! vas por buen camino '$REPLY'."
-    else
-        itentosrestantes=$(( itentosrestantes - 1 ))
-        echo "La letra '$REPLY' no está en la palabra, -1 vida."
-    fi
-    echo
-done
-
-if [[ $adivinarpalabra == $palabra ]]; then
-    echo "¡WUUUUUUU! Adivinaste la palabra: $palabra"
-else
-    echo "chin :(, no adivinaste la palabra correcta era: $palabra"
-fi
+ '
+     _____
+    |   \|
+    |    |
+    |    |
+         |
+         |
+ ________|____
+ 
+ 
+    _____
+    |   \|
+    |    |
+    0    | ERROR. Esa letra no es correcta
+         |
+         |
+ ________|____
+ 
+    
+    _____
+    |   \|
+    |    |
+    0    |
+    |    | ERROR 2. Esa letra no es correcta
+         |
+ ________|____
+ 
+ 
+    _____
+    |   \|
+    |    |
+    0    |
+   /|\    | ERROR 3. Ya tienes tres errores, ya tienes casi la mitad del cuerpo ¡¡CUIDADO!!
+         |
+ ________|____
+ 
+ 
+    _____
+    |   \|
+    |    |
+    0    |
+   /|\   |
+   /     | ERROR 4. Te queda una oprtunidad más, TÚ PUEDES VAMOS, salva a Panchito
+ ________|____
+ 
+ 
+    _____
+    |   \|
+    |    |
+    0    |
+   /|\   |
+   / \   | ERROR 5. No te tocaba carnal, PERDISTE 
+ ________|____
+ '
+ 
+ clear
+ echo -n "¿QUE PALABRA VAMOS A ADIVINAR?: "
+ read word
+ echo
+# echo "Pulsa enter para continuar..."
+# read continuar
+ clear
+ letra="*"   
+ 
+ fallos=5 
+ 
+ letras=`echo $word | sed "s/[^${letra}]/*"/g` 
+ 
+ sust=$letras
+ 
+ while [ "$sust" != "$word" ]      
+ 
+       do
+ #        clear
+ 	  echo
+          echo "LAS LETRAS SON $sust"
+          echo
+          sed -n "$fallos,$((fallos+7))p" $0
+ 	  echo 
+ 	  echo LETRAS INTRODUCIDAS HASTA AHORA: $a
+ 	  echo 	
+ 	  echo -n "INTRODUCE UNA LETRA: "
+          read letra
+ 	  a=${a}$letra
+ 	  palabra=${letra}$palabra   
+ 			
+          echo
+          existe=`echo ${word} | grep ${letra}` 
+ 
+               if [ "$existe" = "" ]
+                  then
+ 	             echo "Esa letra no es correcta, la letra - ${letra} - no es parte de la palabra"
+ 		     error=$((error + 1))
+                     if [ $error = 5 ]
+                        then
+ #	                   clear
+ 		 	   fallos=$(($fallos+9))
+ 			   sed -n "$fallos,$((fallos+7))p" $0
+ 			   echo "Has cometido $error errores, RIP"
+                           echo
+			   echo	
+			   exit
+                        else
+                           if [ $error = 1 ]
+                              then
+                                 echo "Has cometido $error errores"
+ 			         fallos=$(($fallos+9))
+                              else
+ 			         fallos=$(($fallos+9))		
+                           fi
+                     fi
+ 
+ 		  else
+                     sust=`echo $word | sed "s/[^${palabra}]/*"/g` 
+               fi
+ 
+                  if [ "${sust}" = "${word}" ] 
+                     then
+                        echo "¡¡¡FELICIDADES!!! HAS DESCUBIERTO LA PALABRA SECRETA QUE ERA: ${word}."
+                	echo
+ 			echo	
+                  fi
+ 
+       done
