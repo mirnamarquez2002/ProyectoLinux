c5292ee05260054852ffb723c10051abb8c3c2e3
HugoARM <89823278+HugoARM@users.noreply.github.com>
2023-04-22 23:25:44 -0600

    Add files via upload

---
+++ Proyecto Shell Script/ayuda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rojo="\033[31m"
+verde="\033[32m"
+azul="\033[34m"
+reset_color="\033[0m"
+
+echo -e "${rojo}Los comandos disponibles son:"
+echo -e "${azul}ayuda${reset_color}: Proporciona informacion acerca de los comandos disponibles" 
+echo -e "${azul}infosis${reset_color}: Proporciona informacion del sistema" 
+echo -e "${azul}infodia${reset_color}: Proporciona la fecha y hora actuales"
+echo -e "${azul}busca${reset_color}: Busca un archivo en un directorio específico"
+echo -e "${azul}jugar${reset_color}: Ejecuta el juego del sistema"
+echo -e "${azul}creditos${reset_color}: Muestra los creditos del programa"
+echo -e "${azul}MP3${reset_color}: Abrira un reproductor de mp3"
